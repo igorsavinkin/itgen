d52d5800b3a8385d241f7a360bd7430db1062883
Igor Savinkin <igor.savinkin@gmail.com>
2022-06-06 11:08:03 +0300

    for,while

---
+++ filip/if.py
@@ -0,0 +1,16 @@
+a=9
+if a%9==0:
+    if a>9 and a<100:
+        print(1)
+        print(2)
+    else:
+        print(3)
+        print(4)
+else:
+    if s%2==0:
+        print(5)
+        print(6)
+    else:
+        print(7)
+        print(8)
+print("end")
