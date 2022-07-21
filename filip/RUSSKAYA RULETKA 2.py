3eccd08423fbb84ae5b2e9fc976ff3237c7b5894
Igor Savinkin <igor.savinkin@gmail.com>
2022-07-21 17:00:59 +0300

    ruskaya ruletka 2

--- filip/RUSSKAYA RULETKA 2.py
+++ filip/RUSSKAYA RULETKA 2.py
@@ -12,46 +12,35 @@ while  players_counter > marbeles_counter // 2 or  players_counter < 2:
     players_counter = int(input('Введите заново число игроков :  '))
     
 
-
 for i in range(1,players_counter+1):    
     players.append( input(f'Введите имя игрока #{i}: '))
 print(players)
 taken = 0
 current_player = 0
-if players != 2:
-    marbles.remove('green')
-    marbles.append('blue')
+
     
 #print(marbles)
 
 print('''Добро пожаловать в игру “Русская рулетка”!
-    Правила игры: Из меш­ка с пятью зелеными шариками и одним белым шариком
+Правила игры: Из мешка с N зелеными шариками и одним белым шариком
 каждый игрок по очереди достает по одному шарику.
-Если достался зеленый шарик - игра идет дальше,
-если кому-нибудь из игроков достался белый, то тебя убивают с пистолета
-Удачи! ТАКОВА ЖИЗНЬ !!!''')
+Если достался зеленый шарик - игра идет дальше и шарик возвращается в мешок.
+Если кому-нибудь из игроков достался белый, то он проиграл.''')
 while taken != marbeles_counter :
     if current_player >= players_counter:
         current_player = 0
-    u = input(f'''oчередь игрока {players[current_player]}
-Нажми Enter, Чтобы достать шарик ...''')
+    u = input(f'''Очередь игрока {players[current_player]}
+    Нажмите "Enter" чтобы снова достать шарик.''')
     chosen_marble = random.choice(marbles)
-    marbles.remove(chosen_marble)
     if chosen_marble == 'white':
         print('Игрок достал белый шарик')
         print(f'''Игрок под именем {players[current_player]} уже труп СКОРО НАМЕЧАЮТСЯ ПОХОРОНЫ''')
         break
-    elif chosen_marble == 'blue':
-        print(f'''Попался синий шарик
-игрок { players[current_player]} выбывает''')
-        players.remove(players[current_player])
-        players_counter -= 1
     else:
         print('Всё хорошо, Вам попался зелёный шарик.')
         print('Ход переходит к следушему игроку.')
-        taken += 1
         current_player +=1
-    
+        random.shuffle(marbles)  
         
     
     
